6fba3a4df7e999c698f65dd96424121b63a86b21
Taurouf <tauroufouf@gmail.com>
2020-10-15 17:05:40 +0200

    chore(my-react): add file

---
+++ src/components/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = ({ data }) => (
+  <li>
+    <span>{`name : ${data.name} | `}</span>
+    <span>{`phone : ${data.phone}`}</span>
+  </li>
+);
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-const App = () => <div></div>;
+import Home from './components/home';
+
+const datas = [{
+  id: '12345',
+  name: 'greg',
+  phone: '0123433443',
+}, {
+  id: '1234',
+  name: 'taurouf',
+  phone: '07070707',
+}, {
+  id: '123',
+  name: 'gjut',
+  phone: '0808080808',
+},
+];
+
+const App = () => (
+  <div>
+    <ul>
+      {datas.map((item) => <Home data={item} />)}
+    </ul>
+  </div>
+);
+
 ReactDOM.render(<App />, document.getElementById('app'));
